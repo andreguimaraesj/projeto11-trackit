a8d6a41fe2b513debc6c21a933d4c8e59d431daa
andreguimaraesj <dev.detto@gmail.com>
2023-06-02 23:06:56 -0300

    pages and progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './pages/Login'
+import Registration from './pages/Registration'
+import Habits from './pages/Habits'
 
 function App() {
 
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+      <Habits />
+      {/* <Registration /> */}
     </>
   )
 }
---
+++ src/pages/components/header.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components"
+import profilepic from "../../assets/profilepic.png"
+
+export default function Header(){
+    return(
+        <HeaderDiv>
+            <h1>TrackIt</h1>
+            <img src={profilepic}></img>
+        </HeaderDiv>
+    )
+}
+
+const HeaderDiv = styled.div`
+    width: 100%;
+    height: 70px;
+    padding: 10px 20px 10px 20px;
+    align-items: center;
+    background: #126BA5;
+    display: flex;
+    justify-content: space-between;
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    h1{
+        font-family: 'Playball', cursive;
+        color: #ffffff;
+        font-size: 39px;
+    }
+
+    img{
+        width: 50px;
+        border-radius: 30px;
+    }
+`;
