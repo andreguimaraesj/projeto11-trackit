8fc668249a41640c13b6350529d005ccd05e1377
andreguimaraesj <dev.detto@gmail.com>
2023-06-04 20:13:44 -0300

    w/ delete

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import ResetStyle from "./constants/ResetStyle.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <ResetStyle /> */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
